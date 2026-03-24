4dbdbe65b8a4ba688e213153dbd57e08cdafa4c7
Alvaro Ventura <alvarov2@uci.edu>
2026-03-24 07:26:50 -0700

    plz

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path' // ✅ ADD THIS
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
